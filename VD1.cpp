1f9155a4dddf0d18f5debb5cd905cbec1f593f4a
minhkq3009 <55178535+minhkq3009@users.noreply.github.com>
2023-05-11 09:56:32 +0700

    Add files via upload

---
+++ VD1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+int a,b, Tong, Hieu, Tich, Thuong;
+printf("Nhap so a = ");
+scanf("%d", &a);
+printf("Nhap so b = ");
+scanf("%d", &b);
+Tong = a+b;
+Hieu = a-b;
+Tich = a*b;
+Thuong = a/b;
+printf("\nTong 2 so la: %d", Tong);
+printf("\nHieu 2 so la: %d", Hieu);
+printf("\nTich 2 so la: %d", Tich);
+printf("\nThuong 2 so la: %d", Thuong);
+return 0;
+}
+
+
+
+
+
